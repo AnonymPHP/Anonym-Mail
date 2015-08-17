47b736a08f4bfd698ccd6f534c6b40744e4e7970
anonymframework <serif.saglam119@gmail.com>
2015-08-18 02:58:16 +0300

    added swift attachment class

---
+++ SwiftAttachment.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+namespace Anonym\Components\Mail;
+
+/**
+ * the class of swift attachment
+ *
+ * Class SwiftAttachment
+ * @package Anonym\Components\Mail
+ */
+class SwiftAttachment
+{
+
+}
